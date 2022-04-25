983032412b35b87f3226573f9bac55030ed492b7
Youssef Hajjari <youssefhajjari01@gmail.com>
2022-04-25 23:58:33 +0000

    update entry file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  apps : [{
-    name: "app",
-    script: "build/server.js",
-    instances: "max",
-    max_restarts: 5,
-    env: {
-      NODE_ENV: "development",
+  apps: [
+    {
+      name: 'app',
+      script: 'build/main.js',
+      instances: 'max',
+      max_restarts: 5,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
     },
-    env_production: {
-      NODE_ENV: "production",
-    }
-  }]
-}
+  ],
+};
